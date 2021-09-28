ec431a80439ccb387db0807afb82ab816c9ca76f
Marcos Feijó de Almeida <ymfeijo@gmail.com>
2021-09-27 23:28:47 -0300

    Projeto básico inicial

---
+++ src/main/java/br/dev/mfalm/springdataresterrorhandling/SpringDataRestErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mfalm.springdataresterrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/mfalm/springdataresterrorhandling/SpringDataRestErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mfalm.springdataresterrorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
